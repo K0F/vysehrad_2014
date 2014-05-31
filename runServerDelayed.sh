e4b59a92c7b2b8e7e16c1aa990febfc3d25139d4
Kof <krystof.pesek@gmail.com>
2014-05-31 07:41:48 +0200

    change @ production machine Sat May 31 07:41:48 CEST 2014

---
+++ runServerDelayed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sleep 8
+xinit &
+sleep 1
+export DISPLAY=:0.0
+xset s off
+xset -dpms
+xdotool mousemove 1024 768
+rm /tmp/ctl || echo "Fifo is not there.. OK"
+cd /home/kof/vysehrad/prehravac
+/home/kof/bin/pp &
