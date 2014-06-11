10cab5f9b567278cbaf5b15d7204cc1c928da28a
Kof <krystof.pesek@gmail.com>
2014-06-11 22:41:28 +0200

    change @ production machine Wed Jun 11 22:41:28 CEST 2014

--- runServer.sh
+++ runServer.sh
@@ -4,6 +4,7 @@ sleep 1
 export DISPLAY=:0.0
 xset s off
 xset -dpms
+xrandr --output CRT1 --mode 1280x720 --primary --output DFP1 --right-of CRT1 --mode 1920x1080
 #xrandr --output VGA-0 --mode 1280x720 --output HDMI-0 --right-of VGA-0 --mode 1920x1080
 xdotool mousemove 1024 768
 rm /tmp/ctl || echo "Fifo is not there.. OK"
--- runServerDelayed.sh
+++ runServerDelayed.sh
@@ -7,7 +7,7 @@ sleep 1
 export DISPLAY=:0.0
 xset s off
 xset -dpms
-#xrandr --output VGA-0 --mode 1280x720 --output HDMI-0 --right-of VGA-0 --mode 1920x1080
+xrandr --output CRT1 --mode 1280x720 --primary --output DFP1 --right-of CRT1 --mode 1920x1080
 xdotool mousemove 1280 720
 rm /tmp/ctl || echo "Fifo is not there.. OK"
 cd /home/kof/vysehrad/prehravac
